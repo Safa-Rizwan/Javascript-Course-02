a88dd82db87c598584498c03238d51e5454ce7a9
Safa Rizwan <safa4302228@cloud.neduet.edu.pk>
2022-10-22 17:29:56 +0500

    Class 1

---
+++ Class 1/index.js
@@ -0,0 +1,39 @@
+console.log('Hello world')
+
+// console.log() --> print = when we want output on console
+
+console.log(1234)
+console.log('Safa 123 * + ! Rizwan')
+
+// strings---> like alphabets/words/symbols/characters
+
+// numbers ----> numbers
+console.log(23+40);
+console.log(23-12);
+console.log('23+40');
+
+console.log('The result of multiplication is :',4*8)
+console.log('the result of addition is:',23+50)
+// tasks
+// Display names of 3 movies & 3 hobbies on console.
+
+// alert("This is alert")
+// Use when we want to give message to user 
+
+// alert("you are using a website")
+
+prompt("Enter your name")
+
+
+var number = 10;
+var fruit = 'apple'; 
+var name = 'Safa Rizwan';
+console.log(fruit);
+console.log(number);
+
+
+
+
+
+
+
